ab4f84849bce97179f9eb52a058c09fe04b6a693
Damian Bartosiak <bartosiak.contact@gmail.com>
2024-02-14 21:04:55 +0100

    moved all files to backend folder for better readability

---
+++ backend/app.js
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors";
+import db from "./db.js";
+import { appConfig } from "./config/config.js";
+import ErrorHandler from "./middlewares/ErrorHandler.js";
+import replacementRouter from "./router/replacementRouter.js";
+import classroomRouter from "./router/classroomRouter.js";
+import headerRouter from "./router/headerRouter.js";
+import informationRouter from "./router/informationRouter.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+try {
+    await db.authenticate();
+    console.log(
+        "Connection to the database has been established successfully."
+    );
+} catch (error) {
+    console.error("Unable to connect to the database:", error);
+}
+
+app.use("/replacements", replacementRouter);
+app.use("/classrooms", classroomRouter);
+app.use("/headers", headerRouter);
+app.use("/informations", informationRouter);
+
+app.use(ErrorHandler);
+app.listen(appConfig.port, () => {
+    console.log(`Serwer działa na http://localhost:${appConfig.port}`);
+});
