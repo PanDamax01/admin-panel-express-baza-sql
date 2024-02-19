f295254486ec91f422caa900748e746674ad4cfc
Damian Bartosiak <bartosiak.contact@gmail.com>
2024-02-19 23:42:37 +0100

    added the ability to update replacements

--- front-end/src/components/FormRows/FormRows.jsx
+++ front-end/src/components/FormRows/FormRows.jsx
@@ -1,52 +1,87 @@
-import styles from './FormRows.module.scss'
-
-import { FormLabel } from '../FormLabel/FormLabel'
+import styles from "./FormRows.module.scss";
+import { BACK_END_URL } from "../../constants/api";
+import { FormLabel } from "../FormLabel/FormLabel";
+import { Form } from "react-router-dom";
 
 const inputFields = [
-	{ className: `${styles.input} ${styles.short}`, key: 1, name: 'lesson' },
-	{ className: `${styles.input} ${styles.long}`, key: 2, name: 'teacher' },
-	{ className: `${styles.input} ${styles.mid}`, key: 3, name: 'branch' },
-	{ className: `${styles.input} ${styles.long}`, key: 4, name: 'subject' },
-	{ className: `${styles.input} ${styles.mid}`, key: 5, name: 'room' },
-	{ className: `${styles.input} ${styles.long}`, key: 6, name: 'deputy' },
-	{ className: `${styles.input} ${styles.long}`, key: 7, name: 'date' },
-]
+    { className: `${styles.input} ${styles.short}`, key: 1, name: "lesson" },
+    { className: `${styles.input} ${styles.long}`, key: 2, name: "teacher" },
+    { className: `${styles.input} ${styles.mid}`, key: 3, name: "branch" },
+    { className: `${styles.input} ${styles.long}`, key: 4, name: "subject" },
+    { className: `${styles.input} ${styles.mid}`, key: 5, name: "room" },
+    { className: `${styles.input} ${styles.long}`, key: 6, name: "deputy" },
+    { className: `${styles.input} ${styles.long}`, key: 7, name: "date" },
+];
 
+export async function updatedReplacement(args) {
+    const data = Object.fromEntries(await args.request.formData());
+
+    try {
+        const response = await fetch(
+            `${BACK_END_URL}/replacements/${data.id}`,
+            {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data),
+            }
+        );
+
+        const replacement = await response.json();
+        return replacement;
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 export function FormRows({ replacementsData }) {
-	return (
-		<>
-			{replacementsData.map((input, index) => (
-				<div key={input.replacements_id} className={styles.container}>
-					<form className={styles.form}>
-						<input type='hidden' value={input.replacements_id} name='id' />
+    return (
+        <>
+            {replacementsData.map((input, index) => (
+                <div key={input.replacements_id} className={styles.container}>
+                    <Form
+                        className={styles.form}
+                        method="PUT"
+                        action="/panel/zastepstwa"
+                    >
+                        <input
+                            type="hidden"
+                            value={input.replacements_id}
+                            name="id"
+                        />
 
-						<FormLabel id={`lesson${input.replacements_id}`}>
-							Wiersz {index + 1}
-						</FormLabel>
+                        <FormLabel id={`lesson${input.replacements_id}`}>
+                            Wiersz {index + 1}
+                        </FormLabel>
 
-						<div>
-							{inputFields.map((el) => (
-								<input
-									className={el.className}
-									name={`${el.name}${input.replacements_id}`}
-									id={`${el.name}${input.replacements_id}`}
-									placeholder={input[el.name]}
-									key={el.key}
-									type='text'
-								/>
-							))}
-						</div>
+                        <div>
+                            {inputFields.map((el) => (
+                                <input
+                                    className={el.className}
+                                    name={el.name}
+                                    id={`${el.name}${input.replacements_id}`}
+                                    placeholder={input[el.name]}
+                                    defaultValue={input[el.name]}
+                                    key={el.key}
+                                    type="text"
+                                />
+                            ))}
+                        </div>
 
-						<button className={styles.update}>Aktualizuj</button>
-					</form>
+                        <button className={styles.update}>Aktualizuj</button>
+                    </Form>
 
-					<form className={styles.form}>
-						<input type='hidden' value={input.replacements_id} name='id' />
-						<button className={styles.delete}>Usuń</button>
-					</form>
-				</div>
-			))}
-		</>
-	)
+                    <form className={styles.form}>
+                        <input
+                            type="hidden"
+                            value={input.replacements_id}
+                            name="id"
+                        />
+                        <button className={styles.delete}>Usuń</button>
+                    </form>
+                </div>
+            ))}
+        </>
+    );
 }
--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,54 +1,56 @@
-import './styles/theme.scss'
-import './styles/globals.scss'
+import "./styles/theme.scss";
+import "./styles/globals.scss";
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { LoginPage } from './views/LoginPage/LoginPage.jsx'
-import { Layout } from './components/Layout/Layout.jsx'
-import { HeaderPage } from './views/HeaderPage/HeaderPage.jsx'
-import { ReplacementsPage } from './views/ReplacementsPage/ReplacementsPage.jsx'
-import { replacementsLoader } from './api/loaders/replacementsLoader.js'
-import { headerLoader } from './api/loaders/headerLoader.js'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { LoginPage } from "./views/LoginPage/LoginPage.jsx";
+import { Layout } from "./components/Layout/Layout.jsx";
+import { HeaderPage } from "./views/HeaderPage/HeaderPage.jsx";
+import { ReplacementsPage } from "./views/ReplacementsPage/ReplacementsPage.jsx";
+import { replacementsLoader } from "./api/loaders/replacementsLoader.js";
+import { headerLoader } from "./api/loaders/headerLoader.js";
+import { updatedReplacement } from "./components/FormRows/FormRows.jsx";
 
 const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <App />,
-	},
-	{
-		path: '/login',
-		element: <LoginPage />,
-	},
-	{
-		path: '/panel',
-		element: <Layout />,
-		children: [
-			{
-				path: '',
-				element: <p>home</p>,
-			},
-			{
-				path: '/panel/naglowek',
-				element: <HeaderPage />,
-				loader: headerLoader
-			},
-			{
-				path: '/panel/zastepstwa',
-				element: <ReplacementsPage />,
-				loader: replacementsLoader
-			},
-			{
-				path: '/panel/slider',
-				element: <p>slider</p>,
-			},
-		],
-	},
-])
+    {
+        path: "/",
+        element: <App />,
+    },
+    {
+        path: "/login",
+        element: <LoginPage />,
+    },
+    {
+        path: "/panel",
+        element: <Layout />,
+        children: [
+            {
+                path: "",
+                element: <p>home</p>,
+            },
+            {
+                path: "/panel/naglowek",
+                element: <HeaderPage />,
+                loader: headerLoader,
+            },
+            {
+                path: "/panel/zastepstwa",
+                action: updatedReplacement,
+                element: <ReplacementsPage />,
+                loader: replacementsLoader,
+            },
+            {
+                path: "/panel/slider",
+                element: <p>slider</p>,
+            },
+        ],
+    },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
-		<RouterProvider router={router}></RouterProvider>
-	</React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router}></RouterProvider>
+    </React.StrictMode>
+);
