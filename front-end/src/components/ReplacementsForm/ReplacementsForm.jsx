cf78b2820413a807e1e244656b52090d0804ee8f
Damian Bartosiak <bartosiak.contact@gmail.com>
2024-02-20 22:07:26 +0100

    added the ability to create replacements

--- front-end/src/components/ReplacementsForm/ReplacementsForm.jsx
+++ front-end/src/components/ReplacementsForm/ReplacementsForm.jsx
@@ -1,19 +1,20 @@
-import styles from './ReplacementsForm.module.scss'
+import styles from './ReplacementsForm.module.scss';
 
-import { FormRows } from '../FormRows/FormRows'
-import { Title } from '../Title/Title'
+import { FormRows } from '../FormRows/FormRows';
+import { Title } from '../Title/Title';
+import { Form } from 'react-router-dom';
 
 export function ReplacementsForm({ replacementsData }) {
-	return (
-		<>
-			<Title>Edycja</Title>
-			{/* <Checkbox hidden={hidden.hideReplacement} type='hideReplacement' /> */}
+    return (
+        <>
+            <Title>Edycja</Title>
+            {/* <Checkbox hidden={hidden.hideReplacement} type='hideReplacement' /> */}
 
-			<form>
-				<button className={styles.add}>Dodaj wiersz</button>
-			</form>
+            <Form method='POST' action='/panel/zastepstwa'>
+                <button className={styles.add}>Dodaj wiersz</button>
+            </Form>
 
-			<FormRows replacementsData={replacementsData} />
-		</>
-	)
+            <FormRows replacementsData={replacementsData} />
+        </>
+    );
 }
